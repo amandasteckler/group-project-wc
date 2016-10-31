c3c40e57d5ad101a5a8f72028fee53a5fcb24700
krissasantos <krissa.santos31@gmail.com>
2016-10-31 11:28:47 -0400

    add wine & cheese seeds

--- wine-cheese/app/models/post.rb
+++ wine-cheese/app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ApplicationRecord
+  # attr_accessible :categories_attributes, :regions_attributes, :wine_brands_attributes, :cheese_brands_attributes, :wines_attributes, :cheeses_attributes, :pairings_attributes
   has_many :reviews
   has_many :reviewers, through: :reviews
   belongs_to :poster
   belongs_to :pairing
+  # accepts_nested_attributes_for :categories, :regions, :wine_brands, :cheese_brands, :wines, :cheeses, :pairings
+
 end
+
+
+
+#features to add for post per user
+  #rating
+
+#features to add for general website
+  #Search Function via wine
+    #Picking a particular bottle of wine will return the most popular cheese
--- wine-cheese/db/schema.rb
+++ wine-cheese/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031131637) do
+ActiveRecord::Schema.define(version: 20161031145957) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20161031131637) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "company"
   end
 
   create_table "wines", force: :cascade do |t|
