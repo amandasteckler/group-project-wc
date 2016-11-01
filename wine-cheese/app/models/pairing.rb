3cb52bacf518e3934aefae36170ad8e80c7f84fc
Amanda Steckler <amandajsteckler@gmail.com>
2016-11-01 17:41:03 -0400

    CSS stuff

--- wine-cheese/app/models/pairing.rb
+++ wine-cheese/app/models/pairing.rb
@@ -5,6 +5,6 @@ class Pairing < ApplicationRecord
   belongs_to :cheese
 
   def pairing_with_wine_cheese_names
-    Wine.find(self.wine_id).wine_brand.company + " " + Wine.find(self.wine_id).category.name + " + " + Cheese.find(self.cheese_id).cheese_brand.name + " " + Cheese.find(self.cheese_id).name
+    Wine.find(self.wine_id).wine_brand.company + " " + Wine.find(self.wine_id).wine_brand.name + " " + Wine.find(self.wine_id).category.name + " + " + Cheese.find(self.cheese_id).cheese_brand.name + " " + Cheese.find(self.cheese_id).name
   end
 end
